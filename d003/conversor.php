fdb25265a4ce63a90fe458aa9ebe18bfd9534155
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2023-05-15 20:09:17 -0300

    desafio 03: conversor de moedas

---
+++ d003/conversor.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Conversor de moedas</title>
+</head>
+<body>
+    <header>
+        <h1>Conversor de moedas V1.0</h1>
+    </header>
+    <main>
+        <?php
+        $val = $_GET["valor"] ?? 0.0;
+        $dolar = $val / 4.89;
+        $f_val = number_format($val, 2, ",", ".");
+        $f_dol = number_format($dolar, 2, ",", ".");
+        echo "<p>Seus R$ $f_val equivalem a <strong>US$ $f_dol</strong></p>";
+        echo "<p>*Cotação fixa de R$ 4,89 informada diretamente no código</p>"
+        ?>
+        <button><a href="index.html">Voltar</a></button>
+    </main>
+</body>
+</html>
